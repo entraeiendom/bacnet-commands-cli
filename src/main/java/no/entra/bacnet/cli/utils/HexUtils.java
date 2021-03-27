702eee255f2b79075f9719aa81f852fdd4119005
baardl <bard.lind@gmail.com>
2021-03-27 15:47:22 +0100

    HexString to Byte array.

--- src/main/java/no/entra/bacnet/cli/utils/HexUtils.java
+++ src/main/java/no/entra/bacnet/cli/utils/HexUtils.java
@@ -14,4 +14,14 @@ public class HexUtils {
         int length = HexUtils.toInt(lenghtHex);
         return length * 2;
     }
+
+    public static byte[] hexStringToByteArray(String hexString) {
+        int len = hexString.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
+                    + Character.digit(hexString.charAt(i+1), 16));
+        }
+        return data;
+    }
 }
--- src/test/java/no/entra/bacnet/cli/utils/HexUtilsTest.java
+++ src/test/java/no/entra/bacnet/cli/utils/HexUtilsTest.java
@@ -2,10 +2,10 @@ package no.entra.bacnet.cli.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static no.entra.bacnet.cli.utils.HexUtils.findMessageLength;
-import static no.entra.bacnet.cli.utils.HexUtils.toInt;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
+
+import static no.entra.bacnet.cli.utils.HexUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HexUtilsTest {
 
@@ -22,5 +22,13 @@ class HexUtilsTest {
         String expectedContent = "810a002a01040005020109121c020003e92c0080000139004e09552e44400000002f096f2e8204002f4f";
         String bacnetMessageAsHex = expectedContent + "00000000000000000000000000000000000";
         assertEquals(expectedContent.length(), findMessageLength(bacnetMessageAsHex));
+    }
+
+    @Test
+    void hexStringToByteArrayTest() {
+        String hexString = "810b000c0120ffff00ff1008";
+        byte[] bytesAsIntArr = hexStringToByteArray(hexString);
+        byte[] expectedArray = new byte[] {-127, 11, 0, 12, 1, 32, -1, -1, 0, -1, 16, 8};
+        assertTrue(Arrays.equals(expectedArray, bytesAsIntArr));
     }
 }
