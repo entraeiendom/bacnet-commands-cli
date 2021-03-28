2955754ea4f59d2a95d72461e88fd19c44c1a6bb
baardl <bard.lind@gmail.com>
2021-03-28 16:14:47 +0200

    Minor cleanup

--- src/main/java/no/entra/bacnet/cli/Bacnet.java
+++ src/main/java/no/entra/bacnet/cli/Bacnet.java
@@ -9,7 +9,6 @@ import picocli.CommandLine.Command;
 
 /**
  * Bacnet Commands
- *
  */
 @Command(name = "bacnet", mixinStandardHelpOptions = true, version = "0.1.0",
         description = "Find devices, read their names and get present value from sensors.",
@@ -19,26 +18,11 @@ import picocli.CommandLine.Command;
                 DevicesCommand.class,
                 BacnetListen.class
         })
-//class Bacnet implements Callable<Integer> {
-
-//    @Parameters(index = "0", description = "The file whose checksum to calculate.")
-//    private File file;
 class Bacnet {
     @CommandLine.Option(names = {"-ip", "--ipAddress"}, description = "IP Address to the Device")
     private String ipAddress = "127.0.0.1";
     @CommandLine.Option(names = {"-p", "--port"}, description = "Bacnet Port default is 47808")
     private int port = 47808;
-
-//    @Override
-//    public Integer call() throws Exception { // your business logic goes here...
-//        byte[] fileContents = Files.readAllBytes(file.toPath());
-//        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
-//        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
-//        return 0;
-//    }
-
-    // this example implements Callable, so parsing, error handling and handling user
-    // requests for usage help or version help can be done with one line of code.
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new Bacnet()).execute(args);
