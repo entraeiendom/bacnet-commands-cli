b6c5b7b0c2287566874a463d29ce9bec9b26a61e
baardl <bard.lind@gmail.com>
2021-04-01 07:58:27 +0200

    Find devices

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # bacnet-commands-cli
 Find devices, read their names and get present value from sensors.
+
+## Installation
+
+```
+mvn clean install
+./install.sh
+```
+
+## Getting Started
+
+1. Start terminal window
+```
+bacnet listen
+```
+2. Start another terminal window
+```
+bacnet devices find <local broadcast address>
+eg:
+bacnet devices find 192.168.2.255
+```
+
+3. Terminal window no 1:
+```
+list
+```
+Expect output similar to:
+
+```
+List of Devices:
+instance: null, ipAddress: 192.168.2.116, port: 47808, lastSeen: 2021-04-01T05:50:11.222019Z
+instance: 8, ipAddress: 192.168.2.118, port: 47808, lastSeen: 2021-04-01T05:50:11.485128Z
+```
+
--- src/main/java/no/entra/bacnet/cli/device/DevicesCommand.java
+++ src/main/java/no/entra/bacnet/cli/device/DevicesCommand.java
@@ -2,7 +2,12 @@ package no.entra.bacnet.cli.device;
 
 import picocli.CommandLine;
 
+import java.io.IOException;
+import java.net.*;
 import java.util.Locale;
+
+import static no.entra.bacnet.BacnetConstants.BACNET_DEFAULT_PORT;
+import static no.entra.bacnet.cli.utils.HexUtils.hexStringToByteArray;
 
 @CommandLine.Command(name = "devices",
 //        subcommands = { SubcommandAsClass.class, CommandLine.HelpCommand.class },
@@ -11,22 +16,46 @@ public class DevicesCommand {
     @CommandLine.Option(names = {"-ip", "--ipAddress"}, description = "IP Address to the Device")
     private String ipAddress = "127.0.0.1";
     @CommandLine.Option(names = {"-p", "--port"}, description = "Bacnet Port. Default is 47808")
-    private int port = 47808;
+    private int port = BACNET_DEFAULT_PORT;
 
     @CommandLine.Command(name = "find", description = "Find Devices")
     void findDevices(@CommandLine.Parameters(paramLabel = "ipAddress", description = "IP Address to the Device") String ipAddressParam,
-                     @CommandLine.Parameters(paramLabel = "port", description = "Bacnet Port. Default is 47808", arity = "0.1") Integer portParam ) {
+                     @CommandLine.Parameters(paramLabel = "port", description = "Bacnet Port. Default is 47808", arity = "0.1") Integer portParam) {
 
-        if (ipAddressParam != ipAddressParam) {
+        if (ipAddressParam != null) {
             this.ipAddress = ipAddressParam;
         }
         if (portParam != null) {
             this.port = portParam;
         }
 
-        System.out.println(String.format("Find devices using IP %s:%s", ipAddress, port));
+        System.out.println(String.format("Send find devices command using IP %s:%s", ipAddress, port));
+        try {
+            sendWhoIsMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    void sendWhoIsMessage() throws IOException, InterruptedException {
+        String hexString = "810b000c0120ffff00ff1008";
+        SocketAddress inetAddress = new InetSocketAddress(port);
+        InetAddress sendToAddress = InetAddress.getByName(ipAddress);
+        DatagramSocket socket = new DatagramSocket(null);
+        socket.setBroadcast(true);
+        socket.setReuseAddress(true);
+        socket.bind(inetAddress);
+        byte[] buf = hexStringToByteArray(hexString);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, sendToAddress, BACNET_DEFAULT_PORT);
+        System.out.println(String.format("Sending: %s", packet));
+        socket.send(packet);
+        Thread.sleep(100);
+        socket.close();
+    }
+
     /*
     @CommandLine.Command(name = "find", description = "Resolves ISO country codes (ISO-3166-1)")
     void findDevices(
